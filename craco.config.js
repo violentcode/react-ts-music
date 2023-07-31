0177394dbfb545ed038f8ac68583a6d323fffbf9
lyh <476622853@qq.com>
2023-07-31 22:30:00 +0800

    初始化项目

--- craco.config.js
+++ craco.config.js
@@ -1,9 +1,15 @@
 const path = require('path')
+const CracoLessPlugin = require('craco-less')
 
 module.exports = {
   webpack: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
-  }
+  },
+  plugins: [
+    {
+      plugin: CracoLessPlugin
+    }
+  ]
 }
