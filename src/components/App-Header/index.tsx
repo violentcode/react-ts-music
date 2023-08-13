dfe5726bae1318e686ef36e11206fe449e7ac0a9
lyh <476622853@qq.com>
2023-08-14 07:00:20 +0800

    .

--- src/components/App-Header/index.tsx
+++ src/components/App-Header/index.tsx
@@ -1,17 +1,33 @@
 import type { FC, ReactNode } from 'react'
 import { memo } from 'react'
 import { Link } from 'react-router-dom'
-import { HeaderWrapper } from './style'
+import { HeaderWrapper, LeftHeader, RightHeader } from './style'
+import header_title from '@/assects/data/header_title'
 
 interface IProps {
   children?: ReactNode
 }
 const AppHeader: FC<IProps> = () => {
+  function createTab(item: any) {
+    let el: ReactNode
+    switch (item.type) {
+      case 'path':
+        break
+      case 'link':
+        break
+    }
+  }
   return (
     <HeaderWrapper>
-      <Link to="/discover">发现音乐</Link>
-      <Link to="/my">我的音乐</Link>
-      <Link to="/friend">关注</Link>
+      <div className="app-header wrap-v1">
+        <LeftHeader>
+          <a className="logo sprite_01">网易云音乐</a>
+          <div className="list">
+            {header_title.map((item) => createTab(item))}
+          </div>
+        </LeftHeader>
+        <RightHeader>右侧搜索</RightHeader>
+      </div>
     </HeaderWrapper>
   )
 }
--- src/components/App-Header/style.ts
+++ src/components/App-Header/style.ts
@@ -1,3 +1,16 @@
 import styled from 'styled-components'
 
-export const HeaderWrapper = styled.div``
+export const HeaderWrapper = styled.div`
+  height: 70px;
+  background-color: black;
+`
+export const LeftHeader = styled.div`
+  .logo {
+    display: block;
+    background-position: 0 0;
+    width: 176px;
+    height: 70px;
+    text-indent: -9999px;
+  }
+`
+export const RightHeader = styled.div``
