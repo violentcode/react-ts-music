1de3d56a0800daedbd4f15f20ba36c63b0e04f99
lyh <476622853@qq.com>
2023-08-22 22:25:36 +0800

    .

--- src/views/discover/c-views/recommend/c-cpns/slider/index.tsx
+++ src/views/discover/c-views/recommend/c-cpns/slider/index.tsx
@@ -1,8 +1,15 @@
-import type { FC, ReactNode } from 'react'
-import { memo } from 'react'
-import { SliderWrapper } from './style'
+import type { FC, ReactNode, ComponentRef } from 'react'
+import { memo, useRef, useState } from 'react'
+import {
+  ControlWrapper,
+  LeftWrapper,
+  RightWrapper,
+  SliderWrapper
+} from './style'
 import { useAppSelecter } from '@/store'
 import { shallowEqual } from 'react-redux'
+
+import { Carousel } from 'antd'
 
 interface IProps {
   children?: ReactNode
@@ -14,8 +21,43 @@ const Slider: FC<IProps> = () => {
     }),
     shallowEqual
   )
+
+  const [currentSlide, setCurrentSlide] = useState(0)
+  function changeSlider(currentSlide: number) {
+    setCurrentSlide(currentSlide)
+  }
+
+  const carouselRef = useRef<ComponentRef<typeof Carousel>>(null)
+  const handleClickPrev = () => {
+    carouselRef.current?.prev()
+  }
+  const handleClickNext = () => {
+    carouselRef.current?.next()
+  }
+  const bgcImgUrl = bannerList[currentSlide]
+    ? bannerList[currentSlide].imageUrl + '?imageView&blur=40x20'
+    : ''
   return (
-    <SliderWrapper>{bannerList.map((item) => item.imageUrl)}</SliderWrapper>
+    <SliderWrapper bgimgurl={bgcImgUrl}>
+      <div className="inner wrap-v2">
+        <LeftWrapper>
+          <Carousel afterChange={changeSlider} ref={carouselRef} effect="fade">
+            {bannerList.map((item) => {
+              return (
+                <div key={item.targetId} className="item">
+                  <img src={item.imageUrl} />
+                </div>
+              )
+            })}
+          </Carousel>
+        </LeftWrapper>
+        <RightWrapper></RightWrapper>
+        <ControlWrapper>
+          <button className="left" onClick={handleClickPrev}></button>
+          <button className="right" onClick={handleClickNext}></button>
+        </ControlWrapper>
+      </div>
+    </SliderWrapper>
   )
 }
 
--- src/views/discover/c-views/recommend/c-cpns/slider/style.ts
+++ src/views/discover/c-views/recommend/c-cpns/slider/style.ts
@@ -1,3 +1,53 @@
 import { styled } from 'styled-components'
 
-export const SliderWrapper = styled.div``
+interface IProps {
+  bgimgurl: string
+}
+export const SliderWrapper = styled.div<IProps>`
+  background: url(${(props) => props.bgimgurl}) center center / 6000px;
+  height: 285px;
+  position: relative;
+  .inner {
+    display: flex;
+  }
+`
+
+export const LeftWrapper = styled.div`
+  width: 730px;
+  .item img {
+    height: 285px;
+  }
+`
+
+export const RightWrapper = styled.div`
+  width: 250px;
+  height: 285px;
+  background: url(${require('@/assects/img/download.png')});
+`
+
+export const ControlWrapper = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  > button {
+    position: absolute;
+    width: 37px;
+    height: 63px;
+    transform: translateY(-50%);
+    background-color: transparent;
+    cursor: pointer;
+    &:hover {
+      background-color: #999;
+    }
+  }
+  .left {
+    left: -558px;
+    background-image: url(${require('@/assects/img/banner-control-left.png')});
+  }
+
+  .right {
+    right: -558px;
+    background-image: url(${require('@/assects/img/banner-control-right.png')});
+  }
+`
